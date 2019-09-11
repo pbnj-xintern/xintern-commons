567ce89c1370cb6e27274627c702563c373b4eb4
byan1197 <Bondyan97@gmail.com>
2019-09-11 06:20:11 -0400

    added some more fields

--- models/Review.js
+++ models/Review.js
@@ -2,11 +2,18 @@ const mongoose = require('mongoose');
 
 const Review = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    salary: { type: Number, min: 0, max: 5, required: true },
+    salary: { type: Number, required: true },
+    createdAt: { type: mongoose.Schema.Types.Date, required: true },
+    deletedAt: { type: mongoose.Schema.Types.Date, default: null },
     content: { type: String, required: true },
     position: { type: String, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    company: { type: mongoose.Schema.Types.ObjectId, ref: "Company" }
+    company: { type: mongoose.Schema.Types.ObjectId, ref: "Company" },
+    flagged: { type: mongoose.Schema.Types.Boolean },
+    upvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    downvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    sadReacts: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+
 });
 
 module.exports = mongoose.model('Review', Review);
--- models/User.js
+++ models/User.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose');
 
 const User = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    createdAt: { type: mongoose.Schema.Types.Date, required: true },
+    deletedAt: { type: mongoose.Schema.Types.Date, default: null },
+    username: { type: String },
     name: { type: String },
     sex: { type: String },
     photo: { type: String },
     school: { type: String },
     program: { type: String },
-    age: { type: Number }
+    age: { type: Number },
+    isShowInfo: { type: mongoose.Schema.Types.Boolean, default: true }
 });
 
 module.exports = mongoose.model('User', User);
