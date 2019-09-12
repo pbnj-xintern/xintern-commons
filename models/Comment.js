03b0292e17ea30310debf1422e2bd5bea054ba2e
John Cen <john.cen@carloanscanada.com>
2019-09-11 21:14:32 -0400

    Added a createdAt property to Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const Comment = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    createdAt: { type: mongoose.Schema.Types.Date, default: new Date(), required: true },
     parentComment: { type: mongoose.Schema.Types.ObjectId, ref: "Rating" },
     parentReview: { type: mongoose.Schema.Types.ObjectId, ref: "Review" },
     upvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
