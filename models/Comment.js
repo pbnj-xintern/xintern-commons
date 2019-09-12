2a5c5d5f3e6f9c8944482bf69d0bc40c7a372099
John Cen <john.cen@carloanscanada.com>
2019-09-12 14:57:53 -0400

    parentComment should reference the Comment obj
    ?

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Comment = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     createdAt: { type: mongoose.Schema.Types.Date, default: new Date(), required: true },
-    parentComment: { type: mongoose.Schema.Types.ObjectId, ref: "Rating" },
+    parentComment: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
     parentReview: { type: mongoose.Schema.Types.ObjectId, ref: "Review" },
     upvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     downvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
