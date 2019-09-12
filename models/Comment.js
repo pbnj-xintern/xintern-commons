731a18fa1dd187ec2cf4a85a013a6baaa30132fd
byan1197 <Bondyan97@gmail.com>
2019-09-12 19:35:23 -0400

    initial db fix, in preparation for api development

--- models/Comment.js
+++ models/Comment.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const Comment = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     createdAt: { type: mongoose.Schema.Types.Date, default: new Date(), required: true },
-    parentComment: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
-    parentReview: { type: mongoose.Schema.Types.ObjectId, ref: "Review" },
     upvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    downvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+    downvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    parentComment: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
+    flagged: { type: mongoose.Schema.Types.Boolean, default: false }
 });
 
 module.exports = mongoose.model('Comment', Comment);
