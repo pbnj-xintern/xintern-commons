e1628722f6dfa7a483a761430bfa93a3ea86f712
byan1197 <Bondyan97@gmail.com>
2019-09-20 19:41:44 -0400

    aws access keys environment variable changes

--- util/aws.js
+++ util/aws.js
@@ -6,8 +6,8 @@ module.exports.uploadMultipartToS3 = async (fileName, base64Body, path) => {
 
 
     var s3 = new AWS.S3({
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_ACCESS_SECRET
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     });
 
     var params = {
