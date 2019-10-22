0e972c46fb22e886070c59d386e30b90e102cec3
Praiyon <praiyon@gmail.com>
2019-10-22 15:10:18 -0400

    Rating model change

--- models/Rating.js
+++ models/Rating.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const Rating = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    culture: { type: mongoose.Schema.Types.Number, min: 0, max: 5, required: true },
-    mentorship: { type: mongoose.Schema.Types.Number, min: 0, max: 5, required: true },
-    impact: { type: mongoose.Schema.Types.Number, min: 0, max: 5, required: true },
-    interview: { type: mongoose.Schema.Types.Number, min: 0, max: 5, required: true }
+    culture: { type: mongoose.Schema.Types.Number, min: 0, max: 5},
+    mentorship: { type: mongoose.Schema.Types.Number, min: 0, max: 5 },
+    impact: { type: mongoose.Schema.Types.Number, min: 0, max: 5},
+    interview: { type: mongoose.Schema.Types.Number, min: 0, max: 5}
 });
 
 module.exports = mongoose.model('Rating', Rating);
