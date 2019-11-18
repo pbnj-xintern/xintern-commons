ee380b3bbff36ec159fd9dced28657c7cbc65ac9
Praiyon <praiyon@gmail.com>
2019-11-18 17:03:52 -0500

    included error message for unauthorized reqs

--- util/auth_checker.js
+++ util/auth_checker.js
@@ -1,17 +1,19 @@
 const jwt = require('jsonwebtoken');
 const middy = require('middy')
+const Status = require('@pbnj-xintern/xintern-commons/util/status')
 module.exports.verifyJWT = (secret) => {
     return ({
         before: (handler, next) => {
             let token = handler.event.headers.Authorization.replace("Bearer ", "");
+
             try {
                 jwt.verify(token, secret);
                 next()
             } catch (error) {
-                console.log(error)
-                return handler.callback();
+                return handler.callback(null, Status.createErrorResponse(403, "Forbidden"));
             }
         }
+
     })
 };
 
